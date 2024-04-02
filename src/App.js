dd18bb081369e4e283baa0346cb163529ab55aa8
Bassam <bassamyakoub@hotmail.com>
2024-04-02 16:05:20 +0200

    quotes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
+  const [result , setResult] = useState({});
+  const [isClicked ,setIsClicked]=useState(false) ;
+
+
+  const search = async () => {
+    const response = await axios.get('https://api.quotable.io/random');
+  setResult(response.data)
+  }
+
+  const handelSubmit = (e)=>{
+      e.preventDefault();
+      setIsClicked(true);
+    console.log(isClicked)}
+
+  useEffect (() => {
+    if(isClicked){
+      search();
+      setIsClicked(false);
+    }
+  },[isClicked]);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <header className='header'>
+    <h1>Quote Generator</h1>
+  </header>
+  <div className='formbtn'>
+  <form onSubmit={e=> e.preventDefault()} >
+    <button className='btn btn-warning' type='submit' onClick={handelSubmit}>Search</button>
+  </form>
+  </div>
+  <div  className="quote">
+    <h1>{result.content}</h1>
+    <h4>{result.author}</h4>
+  </div>
+  </>
   );
 }
 
